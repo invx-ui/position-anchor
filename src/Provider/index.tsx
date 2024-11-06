becee9f5ec91ed99183f3e386ee23448bb998a52
Kalon Robson <kalon.robson@innovixx.co.uk>
2024-11-06 17:18:28 +0000

    feat: update position prop to support 'fixed' positioning in PositionProviderProps

--- src/Provider/index.tsx
+++ src/Provider/index.tsx
@@ -14,7 +14,7 @@ export type PositionSettings = {
   classPrefix?: string;
   id?: string;
   placement: Position;
-  position?: 'fixed' | 'relative';
+  position?: 'fixed' | 'absolute';
 };
 
 export type PositionProviderProps = PositionSettings & HTMLProps<HTMLDivElement> & {
@@ -26,7 +26,7 @@ export const PositionAnchor: React.FC<PositionProviderProps> = ({
   classPrefix,
   id: idFromProps,
   placement: placementFromProps,
-  position = 'relative',
+  position = 'absolute',
   ...rest
 }) => {
   const [placement, setPlacement] = useState<Position>(placementFromProps);
@@ -55,14 +55,14 @@ export const PositionAnchor: React.FC<PositionProviderProps> = ({
 
       let newStyles = {
         left: 0,
-        position: 'absolute',
+        position,
         top: 0,
       };
 
       const setPosition = (horizontal: number, vertical: number) => {
         newStyles = {
           left: horizontal,
-          position: 'absolute',
+          position,
           top: vertical,
         };
       };
@@ -73,54 +73,97 @@ export const PositionAnchor: React.FC<PositionProviderProps> = ({
         return;
       }
 
-      const relativeTop = anchorRect.top - offsetParentRect.top;
-      const relativeLeft = anchorRect.left - offsetParentRect.left;
-      const relativeRight = offsetParentRect.right - anchorRect.right;
+      if (position === 'fixed') {
+        switch (currentPlacement) {
+          case 'top':
+            setPosition(
+              anchorRect.left + (anchorRect.width - popupRect.width) / 2,
+              anchorRect.top - popupRect.height,
+            );
+            break;
+          case 'right':
+            setPosition(anchorRect.right, anchorRect.top + (anchorRect.height - popupRect.height) / 2);
+            break;
+          case 'bottom':
+            setPosition(
+              anchorRect.left + (anchorRect.width - popupRect.width) / 2,
+              anchorRect.bottom,
+            );
+            break;
+          case 'left':
+            setPosition(anchorRect.left - popupRect.width, anchorRect.top + (anchorRect.height - popupRect.height) / 2);
+            break;
+          case 'center':
+            setPosition(
+              anchorRect.left + (anchorRect.width - popupRect.width) / 2,
+              anchorRect.top + (anchorRect.height - popupRect.height) / 2,
+            );
+            break;
+          case 'bottom-left':
+            setPosition(anchorRect.left, anchorRect.bottom);
+            break;
+          case 'bottom-right':
+            setPosition(anchorRect.right - popupRect.width, anchorRect.bottom);
+            break;
+          case 'top-left':
+            setPosition(anchorRect.left, anchorRect.top - popupRect.height);
+            break;
+          case 'top-right':
+            setPosition(anchorRect.right - popupRect.width, anchorRect.top - popupRect.height);
+            break;
+          default:
+            break;
+        }
+      } else {
+        const relativeTop = anchorRect.top - offsetParentRect.top;
+        const relativeLeft = anchorRect.left - offsetParentRect.left;
+        const relativeRight = offsetParentRect.right - anchorRect.right;
 
-      switch (currentPlacement) {
-        case 'top':
-          setPosition(
-            relativeLeft + (anchorRect.width - popupRect.width) / 2,
-            relativeTop - popupRect.height,
-          );
-          break;
-        case 'right':
-          setPosition(relativeLeft + anchorRect.width, relativeTop + (anchorRect.height - popupRect.height) / 2);
-          break;
-        case 'bottom':
-          setPosition(
-            relativeLeft + (anchorRect.width - popupRect.width) / 2,
-            relativeTop + anchorRect.height,
-          );
-          break;
-        case 'left':
-          setPosition(relativeLeft - popupRect.width, relativeTop + (anchorRect.height - popupRect.height) / 2);
-          break;
-        case 'center':
-          setPosition(
-            relativeLeft + (anchorRect.width - popupRect.width) / 2,
-            relativeTop + (anchorRect.height - popupRect.height) / 2,
-          );
-          break;
-        case 'bottom-left':
-          setPosition(relativeLeft, relativeTop + anchorRect.height);
-          break;
-        case 'bottom-right':
-          setPosition(relativeRight - popupRect.width, relativeTop + anchorRect.height);
-          break;
-        case 'top-left':
-          setPosition(relativeLeft, relativeTop - popupRect.height);
-          break;
-        case 'top-right':
-          setPosition(relativeRight - popupRect.width, relativeTop - popupRect.height);
-          break;
-        default:
-          break;
+        switch (currentPlacement) {
+          case 'top':
+            setPosition(
+              relativeLeft + (anchorRect.width - popupRect.width) / 2,
+              relativeTop - popupRect.height,
+            );
+            break;
+          case 'right':
+            setPosition(relativeLeft + anchorRect.width, relativeTop + (anchorRect.height - popupRect.height) / 2);
+            break;
+          case 'bottom':
+            setPosition(
+              relativeLeft + (anchorRect.width - popupRect.width) / 2,
+              relativeTop + anchorRect.height,
+            );
+            break;
+          case 'left':
+            setPosition(relativeLeft - popupRect.width, relativeTop + (anchorRect.height - popupRect.height) / 2);
+            break;
+          case 'center':
+            setPosition(
+              relativeLeft + (anchorRect.width - popupRect.width) / 2,
+              relativeTop + (anchorRect.height - popupRect.height) / 2,
+            );
+            break;
+          case 'bottom-left':
+            setPosition(relativeLeft, relativeTop + anchorRect.height);
+            break;
+          case 'bottom-right':
+            setPosition(relativeRight - popupRect.width, relativeTop + anchorRect.height);
+            break;
+          case 'top-left':
+            setPosition(relativeLeft, relativeTop - popupRect.height);
+            break;
+          case 'top-right':
+            setPosition(relativeRight - popupRect.width, relativeTop - popupRect.height);
+            break;
+          default:
+            break;
+        }
       }
 
       setPopupStyles({
         left: `${newStyles.left}px`,
-        position: newStyles.position || 'absolute',
+        position,
         top: `${newStyles.top}px`,
       });
     }
@@ -192,7 +235,7 @@ export const PositionAnchor: React.FC<PositionProviderProps> = ({
     <PositionContext.Provider value={context}>
       <div
         style={{
-          position,
+          position: 'relative',
         }}
         {...rest}
       >
